e9156ed9286ab1d41d105bbc1d132cbaaaa90ff4
KajetanRadulski <hesmeron@gmail.com>
2025-09-05 09:43:11 +0200

    Create a culling pass

--- Assets/GPUInstancing/Scripts/RenderFeature/CullingPass.cs
+++ Assets/GPUInstancing/Scripts/RenderFeature/CullingPass.cs
@@ -6,8 +6,7 @@ public class CullingPass : ScriptableRenderPass
 {
     private class PassData
     {
-        //Input buffer handle  that contains all the matrices to be culled
-        public BufferHandle InputBufferHandle;
+        //here will go all the data 
     }
     
     static void ExecutePass(PassData data, ComputeGraphContext context)
@@ -19,22 +18,9 @@ public class CullingPass : ScriptableRenderPass
     public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
     {
         const string passName = "Culling Pass";
-        //We get our input buffer from the static class and import it to get a buffer handle
-        //so that we can use it within the pass
-        BufferHandle inputBufferHandle = renderGraph.ImportBuffer(InstancedDrawSystem.GetInputBuffer());
-        
-        //We get or create an instance of this ContextItem class
-        CullingFrameData cullingFrameData = frameData.GetOrCreate<CullingFrameData>();
-        //We fill in the reference for CuledMatricesBuffer
-        cullingFrameData.CulledMatricesBuffer = inputBufferHandle;    
-        
+            
         using (var builder = renderGraph.AddComputePass<PassData>(passName, out var passData))
         {
-            //We fill in PassData with this buffer hande
-            passData.InputBufferHandle = inputBufferHandle;
-            //We have to declare that we will be using this buffer so it is accessible in this pass
-            //We only need read permissions as we will be not modifying this buffer.
-            builder.UseBuffer(passData.InputBufferHandle, AccessFlags.Read);
             builder.SetRenderFunc((PassData data, ComputeGraphContext context) => ExecutePass(data, context));
         }
     }
