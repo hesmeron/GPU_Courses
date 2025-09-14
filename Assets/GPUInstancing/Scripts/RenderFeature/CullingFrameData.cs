8e1ccdde39679f30eb5471089b909590887edf8c
KajetanRadulski <hesmeron@gmail.com>
2025-09-14 15:14:54 +0200

    Work on using new drawing function

---
+++ Assets/GPUInstancing/Scripts/RenderFeature/CullingFrameData.cs
@@ -0,0 +1,15 @@
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.RenderGraphModule;
+
+//This class has to inherit from ContexItem class
+public class CullingFrameData : ContextItem
+{
+    //We have a variable that holds a reference to the buffer with culled matrices
+    public BufferHandle CulledMatricesBuffer;
+    
+    public override void Reset()
+    {
+        //We reset this itewm by setting CulledMatricesBuffer to null
+        CulledMatricesBuffer = BufferHandle.nullHandle;
+    }
+}
