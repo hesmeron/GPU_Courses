399e2ac699d0779926429c5d31f0ac5cbcf1b2d8
KajetanRadulski <hesmeron@gmail.com>
2025-09-14 14:08:48 +0200

    Create instanced draw system

--- Assets/GPUInstancing/Scripts/RenderFeature/CullingPass.cs
+++ Assets/GPUInstancing/Scripts/RenderFeature/CullingPass.cs
@@ -6,7 +6,8 @@ public class CullingPass : ScriptableRenderPass
 {
     private class PassData
     {
-        //here will go all the data 
+        //Input buffer handle  that contains all the matrices to be culled
+        public BufferHandle InputBufferHandle;
     }
     
     static void ExecutePass(PassData data, ComputeGraphContext context)
@@ -18,9 +19,18 @@ public class CullingPass : ScriptableRenderPass
     public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
     {
         const string passName = "Culling Pass";
+        //We get our input buffer from the static class and import it to get a buffer handle
+        //so that we can use it within the pass
+        BufferHandle inputBufferHandle = renderGraph.ImportBuffer(InstancedDrawSystem.GetInputBuffer());
+
             
         using (var builder = renderGraph.AddComputePass<PassData>(passName, out var passData))
         {
+            //We fill in PassData with this buffer hande
+            passData.InputBufferHandle = inputBufferHandle;
+            //We have to declare that we will be using this buffer so it is accessible in this pass
+            //We only need read permissions as we will be not modifying this buffer.
+            builder.UseBuffer(passData.InputBufferHandle, AccessFlags.Read);
             builder.SetRenderFunc((PassData data, ComputeGraphContext context) => ExecutePass(data, context));
         }
     }
