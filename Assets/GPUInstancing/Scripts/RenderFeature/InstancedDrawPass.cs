84469e6edffc85fa81e8186ea67ec8b74bcdf50b
KajetanRadulski <hesmeron@gmail.com>
2025-09-14 15:14:54 +0200

    Work on using new drawing function

--- Assets/GPUInstancing/Scripts/RenderFeature/InstancedDrawPass.cs
+++ Assets/GPUInstancing/Scripts/RenderFeature/InstancedDrawPass.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.RenderGraphModule;
@@ -5,6 +6,7 @@ using UnityEngine.Rendering.Universal;
 
 class InstancedDrawPass : ScriptableRenderPass
 {
+    private static readonly int TransformationMatrices = Shader.PropertyToID("_TransformationMatrices");
     private Material _material;
     private Mesh _mesh;
 
@@ -16,6 +18,7 @@ class InstancedDrawPass : ScriptableRenderPass
 
     private class PassData
     {
+        public BufferHandle culledMatricesBuffer;
         public Material material;
         public Mesh mesh;
     }
@@ -23,29 +26,30 @@ class InstancedDrawPass : ScriptableRenderPass
     static void ExecutePass(PassData data, RasterGraphContext context)
     {
         int shaderPass = data.material.FindPass("Unlit");
-        Matrix4x4[] matrices = new Matrix4x4[100 * 100];
-        
-        for (int x = 0; x < 100; x++)
-        {
-            for (int z = 0; z < 100; z++)
-            {
-                Matrix4x4 matrix = Matrix4x4.TRS(new Vector3(x*1.2f, 0, z*1.5f), Quaternion.identity, Vector3.one);
-                matrices[x * 100 + z] = matrix;
-            }
-        }
-        context.cmd.DrawMeshInstanced(data.mesh, 0, data.material, shaderPass, matrices);
+        MaterialPropertyBlock block = context.renderGraphPool.GetTempMaterialPropertyBlock();
+        block.SetBuffer(TransformationMatrices, data.culledMatricesBuffer);
+        context.cmd.DrawMeshInstancedProcedural(data.mesh, //a mesh to draw that we get form the inspector
+            0, //relevant when the mesh has multiple submeshes, we just set it to 0
+            data.material, //a material to draw that we get form the inspector
+            shaderPass, //As ina previously used function we set the pass that will be used in rendering
+            10000, //for know we assume all matrices are present here. We will replace this later down the line
+            block) //here, all the material properties are set
+            ;
     }
     
     public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
     {
         const string passName = "Render Custom Pass";
+        CullingFrameData cullingFrameData = frameData.GetOrCreate<CullingFrameData>();
             
         using (var builder = renderGraph.AddRasterRenderPass<PassData>(passName, out var passData))
         {
+            passData.culledMatricesBuffer = cullingFrameData.CulledMatricesBuffer;
             passData.material = _material;
             passData.mesh = _mesh;
             UniversalResourceData resourceData = frameData.Get<UniversalResourceData>();
             
+            builder.UseBuffer(passData.culledMatricesBuffer, AccessFlags.Read);
             builder.SetRenderAttachment(resourceData.activeColorTexture, 0);
             builder.SetRenderAttachmentDepth(resourceData.activeDepthTexture);
             builder.SetRenderFunc((PassData data, RasterGraphContext context) => ExecutePass(data, context));
