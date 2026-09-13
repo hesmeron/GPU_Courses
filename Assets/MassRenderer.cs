f692e03e3a05e49befd747a337c3d465eff54f49
Kajetan Radulski <kajetan.radulski@gmail.com>
2026-09-13 11:58:19 +0200

    Create MassRenderer with Gizmos

---
+++ Assets/MassRenderer.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+
+public class MassRenderer : MonoBehaviour
+{
+    [SerializeField] 
+    private Bounds _bounds;
+
+    [SerializeField] 
+    private float _spacing;
+
+    [SerializeField] 
+    private int _instanceCount;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.color = new Color(0,1,0,0.3f);
+        Gizmos.DrawCube(_bounds.center, _bounds.size);
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(_bounds.center, _bounds.size);
+        
+        Gizmos.color = Color.red;
+
+        Vector3 start = _bounds.min;
+
+        for (int i = 0; i < _instanceCount; i++)
+        {
+            int numberOfColumnsPerRow =  Mathf.FloorToInt(_bounds.size.x / _spacing);
+            int x  = i%numberOfColumnsPerRow;
+            int y = (i -x)/numberOfColumnsPerRow;
+            Vector3 offset = new Vector3(x, 0, y) * _spacing;
+            Vector3 pos = start + offset;
+            Gizmos.DrawSphere(pos, _spacing/2f);
+        }
+    }
+}
